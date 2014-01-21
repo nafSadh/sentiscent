ed0506106e3c84181411007bed93d061772adcd2
nafsadh <nafsadh@2dd2fd02-b4e9-4e9b-8d2e-8178913e83a5>
2014-01-21 22:50:38 +0000

    n/c

--- sentiscent/Miner/Miner/EkwOAnalyze.cs
+++ sentiscent/Miner/Miner/EkwOAnalyze.cs
@@ -4,14 +4,22 @@ using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 using System.Net;
-
+using System.Linq;
+using System.Drawing;
 
 namespace Miner
 {
     class EkwOAnalyze
     {
+        public static bool AllNounsEntity = false;
+        public static bool UsersEntity = true;
+
         public static bool isKeyWord(string pos, string word)
         {
+            if (AllNounsEntity)
+            {
+                if (pos == "NN" || pos == "NNS") return false;
+            }
             switch (pos)
             {
                 case "JJ":	//adjective",	//green
@@ -48,6 +56,13 @@ namespace Miner
         }
         public static bool isEntity(string pos, string word)
         {
+            if (AllNounsEntity)
+            {
+                //if (pos == "NN" || pos == "NNS") return true;
+            }
+
+            if (!UsersEntity && pos == "USR") return false;
+
             switch (pos)
             {
                 case "NNP":
@@ -55,7 +70,7 @@ namespace Miner
                 case "USR":
                     {
                         if (word == "US") return true;
-                        if (word == "RT") return true;
+                        if (word == "RT") return false;
                         word = word.ToLower();
                         if (word == "be" || word == "am" || word == "are" || word == "is" ||
                              word == "was" || word == "were" || word == "been" || word == "being" ||
@@ -77,7 +92,7 @@ namespace Miner
             }
         }
 
-        public static string[] filters = { "@", "#", "’s", "'s", ".", ":", ";", "[", "]", "(", ")", "{", "}", ",", "?", "\"", "!" };
+        public static string[] filters = { "@", "#", "’s", "'s", ".", ":", ";", "[", "]", "(", ")", "{", "}", ",", "?", "\"", "!", "+", "+", "=", "<", ">", "*",":(",":)",":","(",")" };
 
         public static string keyWordiFy(string word)
         {
@@ -120,12 +135,18 @@ namespace Miner
         private Dictionary<string, kwEo> kwEDict;
         private Dictionary<string, kwEo> kwLexicon;
         private Dictionary<string, EEVertex> EkwEgg;
+        private List<Community> Communities;
+        Dictionary<string, int> bfsedE;
+        Dictionary<string, int> sigEntities;
+        Dictionary<string, int> kwFinal;
+
 
         public EkwOAnalyze()
         {
             EkwDict = new Dictionary<string, EkwO>(StringComparer.InvariantCultureIgnoreCase);
             kwEDict = new Dictionary<string, kwEo>(StringComparer.InvariantCultureIgnoreCase);
             kwLexicon = new Dictionary<string, kwEo>(StringComparer.InvariantCultureIgnoreCase);
+            Communities = new List<Community>();
         }
 
         //TakeUp: Tweet, Entity, kw, polarity
@@ -172,7 +193,7 @@ namespace Miner
 
                 //string[] lineParts = line.Split(space, 4);
 
-                string taggedTweet = line;
+                string taggedTweet = System.Text.RegularExpressions.Regex.Replace(line, @"[^\u0000-\u007F]", string.Empty); ;
 
                 string[] tweetParts = taggedTweet.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
                 string tweet = "";
@@ -222,7 +243,7 @@ namespace Miner
                 string marker = "neutral";
                 if (pScore <= -0.45) { marker = "negative"; negCnt++; }
                 else if (pScore >= +0.45) { marker = "positive"; posCnt++; }
-
+                pScore /= 2;
                 totalScore += pScore;
 
                 fs.WriteLine(i + ",\"" + tweet.Replace(",", "_CM_") + "\"," + E + "," + kw + "," + pScore + "," + marker + "," + ngcScore + "," + posScore + "," + sens);
@@ -256,6 +277,7 @@ namespace Miner
             xDoc.Load(TEkwPpath);
             xDoc.Normalize();
             XmlNode root = xDoc.DocumentElement;
+
 
             StreamWriter fse = new StreamWriter(reportPath + ".E.csv", false);
             StreamWriter fsk = new StreamWriter(reportPath + ".kw.csv", false);
@@ -344,38 +366,52 @@ namespace Miner
                     }
                 }
             }
+
+
+            Console.WriteLine("echo time to calculate: " + (TimeSpan)(DateTime.Now-startTime));
+            Console.WriteLine("storing in files");
+            fsk.WriteLine("keyword"+ "," + "occur" + "," + "ln(occur)" + "," + "PScore" + "," + "E count" + "described entities");
             foreach (string kwl in kwLexicon.Keys)
             {
                 int occur = kwLexicon[kwl].Occur = kwEDict[kwl].Occur;
                 kwLexicon[kwl].PScore = kwEDict[kwl].PScore/occur;
-                if (occur > 1) fsk.WriteLine(kwl + "," + occur + "," + Math.Log(occur) + "," + kwLexicon[kwl].PScore + "," + kwLexicon[kwl].E.Count);
+                if (occur > 1)
+                {
+                    /*string all_E = "";
+                    foreach (string an_E in kwLexicon[kwl].E.Keys) all_E += an_E.Replace(",", "") + ";";*/
+                    fsk.WriteLine(kwl + "," + occur + "," + Math.Log(occur) + "," + kwLexicon[kwl].PScore + "," + kwLexicon[kwl].E.Count /*+ "," + all_E*/);
+                }
             }
-            Console.WriteLine(kwCount_f + " kw of " + kwCount_uf);
-            Console.WriteLine(kwLexicon.Count + " kw in lexicon");
+            Console.WriteLine("good freq kw: "+kwCount_f + " all kw(raw count) " + kwCount_uf);
+            Console.WriteLine(kwLexicon.Count + " kw in lexicon, unique kw");
             //review
             int i = 0;
+            fse.WriteLine("Entity" + "," + "occur" + "," + "ln(occur)" + "," + "PScore" + "," + "kw Count" + "," + "keywords");
             foreach (KeyValuePair<string, EkwO> e_ekwo in EkwDict)
             {
                 int occur = e_ekwo.Value.Occur;
-                if (occur > 1)
+                if (occur > 2)
                 {
-                    fse.WriteLine(e_ekwo.Key + "," + occur + "," + Math.Log(occur) + "," + e_ekwo.Value.PScore + "," + e_ekwo.Value.kw.Count);
+                    /*string all_kw = "";
+                    foreach (string a_kw in e_ekwo.Value.kw.Keys) all_kw += a_kw.Replace(",","") + ";";*/
+                    fse.WriteLine(e_ekwo.Key + "," + occur + "," + Math.Log(occur) + "," + e_ekwo.Value.PScore + "," + e_ekwo.Value.kw.Count/*+","+all_kw*/);
                     i++;
                 }
             }
             fse.Close();
             fsk.Close();
-            Console.WriteLine(i + " of " + EkwDict.Count + " E occured more than once");
-
-            DateTime endTime = DateTime.Now;
-            TimeSpan ts = endTime - startTime;
-
-            Console.WriteLine("echo time to calculate: " + ts);
+            Console.WriteLine(i + " of " + EkwDict.Count + " E occured more than twice");
+            Console.WriteLine("echo time to store and calculate: " + (TimeSpan)(DateTime.Now - startTime));
         }
 
-        public void genEEgraph(string reportPath,int kwLimitThreshold=350, int entityOccurThreshold=2, double polarityThresh=0.45, bool PoleInvariant = false)
+        public void genEEgraph(string reportPath,int kwLimitThreshold=350, int entityOccurThreshold=2, double polarityThresh=0.35, bool PoleInvariant = false, bool NoSelfEdge=true)
         {
-            if (EkwEgg == null) { EkwEgg = new Dictionary<string, EEVertex>(); }
+            if (EkwEgg == null) { EkwEgg = new Dictionary<string, EEVertex>(StringComparer.InvariantCultureIgnoreCase); }
+            Console.WriteLine("Generating EE graph");
+
+            int legitKWCnt = 0;
+            foreach (KeyValuePair<string, kwEo> cnt in kwLexicon) { if (cnt.Value.E.Count < kwLimitThreshold)legitKWCnt++; }
+            Console.WriteLine("from " + legitKWCnt + " legit kw");
             DateTime startTime = DateTime.Now;
             foreach(KeyValuePair<string, EkwO> ekwo in EkwDict)
             {
@@ -395,6 +431,7 @@ namespace Miner
                         Dictionary<string,Meta> PotentialNeighbors = kwLexicon[keyword].E;
                         foreach (string nborE in PotentialNeighbors.Keys)
                         {
+                            if (nborE == E && NoSelfEdge) continue;
                             double nborPScore = EkwDict[nborE].PScore;
                             polarity pole = polarity.opn;
                             if (nborPScore >= polarityThresh && pScore >= polarityThresh) { pole = polarity.pos; }
@@ -420,11 +457,27 @@ namespace Miner
                 }//iterating over kw
             }//iterating over EkwDict
 
-            DateTime endTime = DateTime.Now;
-            TimeSpan ts = endTime - startTime;
-            Console.WriteLine("EkwEG time to generate EE graph: " + ts);
+            //cleanup
+            foreach (string node in EkwEgg.Keys)
+            {
+                string[] nborvs = new string[EkwEgg[node].Neighbors.Keys.Count];
+                int i = 0;
+                foreach (string nbk in EkwEgg[node].Neighbors.Keys)
+                {
+                    nborvs[i++] = nbk;
+                }
+                foreach (string nbork in nborvs)
+                {
+                    if (!EkwEgg.ContainsKey(nbork))
+                    {
+                        EkwEgg[node].Neighbors.Remove(nbork);
+                    }
+                }
+            }
+            Console.WriteLine("and " + EkwEgg.Count + " entities");
+            Console.WriteLine("EkwEG time to generate EE graph: " + (TimeSpan)(DateTime.Now - startTime));
 
-            StreamWriter fs = new StreamWriter(reportPath + ".EkwEGg.csv", false);
+            StreamWriter fs = new StreamWriter(reportPath + "-"+ kwLimitThreshold+"," + entityOccurThreshold +".EkwEGg.csv", false);
             foreach (KeyValuePair<string, EEVertex> evert in EkwEgg)
             {
                 fs.Write(evert.Key + ","+evert.Value.Neighbors.Count+",");
@@ -434,7 +487,237 @@ namespace Miner
                 }
                 fs.WriteLine();
             }
+            fs.Close();
+            Console.WriteLine("EkwEG time to store & generate EE graph: " + (TimeSpan)(DateTime.Now - startTime));
         }
+
+        public void Communitize(string filepath,double comThres=0.01, int MinNeighborForSeed = 2, string forcedSeed="")
+        {
+            DateTime startTime = DateTime.Now;
+            Dictionary<string, int> AllEntity = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+
+            foreach (string entity in EkwEgg.Keys)
+            {
+                if (EkwEgg[entity].Neighbors.Count >= MinNeighborForSeed)
+                    if (!AllEntity.ContainsKey(entity))
+                        AllEntity.Add(entity, EkwEgg[entity].Neighbors.Count);
+            }
+
+            bool keepRunning = true;
+
+            string seed = forcedSeed;
+
+            Console.WriteLine("building communities");
+            Console.WriteLine("SignificantEntity.Count: " + AllEntity.Count);
+            while (keepRunning)
+            {
+                if (AllEntity.Count < 1) { keepRunning = false; continue; }
+
+                Community aCommunity = new Community();
+                aCommunity.CommCondThres = comThres;
+                //seed it
+                if (seed == "" || !AllEntity.ContainsKey(seed))
+                    seed = AllEntity.Keys.First();
+                while (EkwEgg[seed].Neighbors.Count < MinNeighborForSeed)
+                {
+                    AllEntity.Remove(seed);
+                    seed = AllEntity.Keys.First();
+                    Console.Write("\r" + AllEntity.Count);
+                }
+
+                Console.WriteLine("\t EntityRemain : " + AllEntity.Count);
+                bool keepBuildingThisComnty = true;
+                
+                string entity = seed;
+                while (keepBuildingThisComnty)
+                {
+                    EEVertex eev = EkwEgg[entity];
+                    Community.EvalVal eval = aCommunity.EvalEEVertex(eev);
+                    if (eval.memberable)
+                    {
+                        aCommunity.AddVertexToCommunity(eev, eval, EkwDict[entity].PScore);
+                        AllEntity.Remove(entity);
+                        Console.Write("\r" + AllEntity.Count);
+                    }
+                    else if (aCommunity.OutlinkedNodes.Count < 1)
+                    {
+                        keepBuildingThisComnty = false;
+                        continue;
+                    }
+                    else
+                    {
+                        //this is not going to be a potential member
+                        aCommunity.OutlinkedNodes.Remove(entity);
+                    }
+                    if (aCommunity.OutlinkedNodes.Count > 0)
+                    {
+                        entity = aCommunity.OutlinkedNodes.Keys.First();
+                    }
+                    else
+                    {
+                        keepBuildingThisComnty = false;
+                        continue;
+                    }
+                }
+                if (aCommunity.Size> 0)
+                {
+                    aCommunity.Consolidate();
+                    Communities.Add(aCommunity);
+                }
+                Console.Write("\tCommunity size:"+ aCommunity.MemberNodes.Count);
+                seed = entity;
+            }
+
+            Console.WriteLine("time to find communitieis: " + (TimeSpan)(DateTime.Now - startTime));
+            int largesComntySz = 0;
+
+            StreamWriter fs = new StreamWriter(filepath);
+            fs.WriteLine("<Communities>");
+            int i = 1;
+            foreach (Community comnty in Communities)
+            {
+                //int kwThreshhold = comnty;
+                fs.WriteLine(" <Community id='"+i+++"' size='"+ comnty.Size +"' conductance='"+comnty.Conductance+"' pScore='"+ comnty.pScore +"'>");
+                fs.WriteLine("  <trapped-keywords count='" + comnty.TrappedKeywords.Count + "'>");
+                fs.Write("       ");
+                foreach (string kw in comnty.TrappedKeywords.Keys)
+                {
+                    fs.Write(System.Net.WebUtility.HtmlEncode(kw)+":"+comnty.TrappedKeywords[kw]+",");
+                }
+                fs.WriteLine("\n    </trapped-keywords>");
+                foreach (string member in comnty.MemberNodes.Keys)
+                {
+                    fs.WriteLine("  <e>" + System.Net.WebUtility.HtmlEncode(member)+ "</e>");
+                }
+                fs.WriteLine(" </Community>");
+
+                largesComntySz = (largesComntySz > comnty.Size) ? largesComntySz : comnty.Size;
+            }
+
+            fs.WriteLine("</Communities>");
+            fs.Close();
+            Console.WriteLine("\n\nCommunities.Count: " + Communities.Count);
+            Console.WriteLine("Largest Communities Size: " + largesComntySz);
+
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(filepath);
+            xDoc.Normalize();
+            xDoc.Save(filepath);
+        }
+
+        public void BFS(string path)
+        {
+            Dictionary<string, int> AllEntity = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+            bfsedE = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+            Queue<string> Q = new Queue<string>();
+            int i = 0;
+            foreach (string entity in EkwEgg.Keys)
+            {
+                if (!AllEntity.ContainsKey(entity))
+                    AllEntity.Add(entity, EkwEgg[entity].Neighbors.Count);
+            }
+
+            bool run = true;
+            string enty = AllEntity.Keys.First();
+            AllEntity.Remove(enty);
+            while (run)
+            {
+                if (Q.Count < 1)
+                {
+                    if (AllEntity.Count < 1) { run = false; continue; }
+                    enty = AllEntity.Keys.First();
+                    AllEntity.Remove(enty);
+                    if (!bfsedE.ContainsKey(enty))
+                        bfsedE.Add(enty,i++);
+                }
+                else
+                {
+                    enty = Q.Dequeue();
+                }
+
+                foreach (string ne in EkwEgg[enty].Neighbors.Keys)
+                {
+                    if (!bfsedE.ContainsKey(ne))
+                    {
+                        AllEntity.Remove(ne);
+                        Q.Enqueue(ne);
+                        bfsedE.Add(ne, i++);
+                    }
+                }
+            }
+            StreamWriter fs = new StreamWriter(path);
+            foreach (string E in bfsedE.Keys)
+            {
+                fs.WriteLine(E+",");
+            }
+            fs.Close();
+        }
+
+        public int FinalKW(string filepath)
+        {
+            kwFinal = new Dictionary<string,int>(StringComparer.CurrentCultureIgnoreCase);
+
+            foreach (Community comnty in Communities)
+            {
+                foreach (string kw in comnty.TrappedKeywords.Keys)
+                {
+                    if (!kwFinal.ContainsKey(kw)) kwFinal.Add(kw, 0);
+                    kwFinal[kw] += comnty.TrappedKeywords[kw];
+                }
+            }
+
+            StreamWriter fs = new StreamWriter(filepath + ".kw_final.csv");
+            fs.WriteLine("kw,Ocurnc");
+            foreach (string kw in kwFinal.Keys)
+            {
+                fs.WriteLine(kw, kwFinal[kw]);
+            }
+            fs.Close();
+            return kwFinal.Count;
+        }
+
+        public void BFS_Image(string path, int minNborCnt = 2)
+        {
+
+            sigEntities= new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+
+            int ix=0;
+            foreach (string entity in bfsedE.Keys)
+            {
+                if (EkwEgg[entity].Neighbors.Count > minNborCnt)
+                {
+                    sigEntities.Add(entity, ix++);
+                }
+            }
+
+            int dim = sigEntities.Count;
+            Bitmap bmp = new System.Drawing.Bitmap(dim, dim);
+            Console.WriteLine("drawing " + dim + "x" + dim + " adj mat image");
+            //init white
+            //for(int i=0;i<dim;i++)
+            //    for(int j=0;j<dim;j++)
+            //        bmp.SetPixel(i,j, Color.White);
+
+            ix = 0;
+            foreach (string entity in sigEntities.Keys)
+            {
+                int x = sigEntities[entity];
+                Console.Write("\r" + ix++);
+                foreach (string nbor in EkwEgg[entity].Neighbors.Keys)
+                {
+                    if (sigEntities.ContainsKey(nbor))
+                    {
+                        int y = sigEntities[nbor];
+                        bmp.SetPixel(x, y, Color.DarkBlue);
+                    }
+                }
+            }
+
+            bmp.Save(path, System.Drawing.Imaging.ImageFormat.Png);
+            Console.WriteLine("\nSaved at: " + path);
+        }
+
+
 
         public static void RemoveDupLines(string filePath, string filteredFilepath)
         {
@@ -451,6 +734,8 @@ namespace Miner
             {
                 fs.WriteLine(line);
             }
+
+            fs.Close();
         }
     }
 }
--- sentiscent/Miner/Miner/EkwOClasses.cs
+++ sentiscent/Miner/Miner/EkwOClasses.cs
@@ -24,8 +24,8 @@ namespace Miner
             get { return _pScore; }
             set { _pScore = value; }
         }
-        
-        
+
+
     }
 
     public class EkwO
@@ -55,8 +55,8 @@ namespace Miner
             get { return _pScore; }
             set { _pScore = value; }
         }
-        
-        
+
+
         //associated keyword
         public Dictionary<string, Meta> kw;
 
@@ -65,7 +65,7 @@ namespace Miner
             E = entity;
             PScore = 0.0;
             Occur = 0;
-            kw = new Dictionary<string, Meta>();
+            kw = new Dictionary<string, Meta>(StringComparer.InvariantCultureIgnoreCase);
         }
 
         public void AddKW(string keyWord, double pScore)
@@ -75,18 +75,18 @@ namespace Miner
             kw[keyWord].PScore += pScore;
         }
 
-        public void freshen(double threshold=0.35)
+        public void freshen(double threshold = 0.35)
         {
             _pScore = _pScore / Occur;
 
-            string [] kwkeys = new string[kw.Keys.Count];
-            
+            string[] kwkeys = new string[kw.Keys.Count];
+
             int i = 0;
             foreach (string keyw in kw.Keys)
             {
                 kwkeys[i++] = keyw;
             }
-            
+
             foreach (string keyw in kwkeys)
             {
                 kw[keyw].PScore /= kw[keyw].Occur;
@@ -138,7 +138,7 @@ namespace Miner
             kw = keyword;
             PScore = 0.0;
             Occur = 0;
-            E = new Dictionary<string, Meta>();
+            E = new Dictionary<string, Meta>(StringComparer.InvariantCultureIgnoreCase);
         }
 
         public void AddEntity(string entity, double pScore)
@@ -159,9 +159,9 @@ namespace Miner
         }
     }
 
-    public enum polarity{neg=1, obj=0, pos=1, opn};
+    public enum polarity { neg = 1, obj = 0, pos = 1, opn };
 
-    public class kwp 
+    public class kwp
     {
         private string _kw;
 
@@ -191,7 +191,7 @@ namespace Miner
         {
             Weight = 0; kw = ""; Pole = pole;
         }
-        
+
     }
 
     public class EEVertex
@@ -209,7 +209,7 @@ namespace Miner
         public EEVertex(string thisEntity)
         {
             Entity = thisEntity;
-            Neighbors = new Dictionary<string, kwp>();
+            Neighbors = new Dictionary<string, kwp>(StringComparer.InvariantCultureIgnoreCase);
         }
 
         public void AddNeighbor(string nbor, string linke_kw, polarity mutual_pole)
@@ -219,6 +219,157 @@ namespace Miner
             Neighbors[nbor].kw += linke_kw + ",";
             Neighbors[nbor].Weight++;
             if (Neighbors[nbor].Pole != mutual_pole) Neighbors[nbor].Pole = polarity.opn;
+        }
+    }
+
+    public class Community
+    {
+        private uint _volume;
+
+        public uint Volume
+        {
+            get { return _volume; }
+            //set { _volume = value; }
+        }
+
+        private uint _outlinks;
+
+        public uint OutlinksCount
+        {
+            get { return _outlinks; }//not equal to OutlinkedNodes.Count
+            //set { _outlinks = value; }
+        }
+
+        private double _pScoreSum;
+
+        public double pScore
+        {
+            get { return _pScoreSum/Size; }
+            //set { _polarity = value; }
+        }
+        
+
+        public double Conductance
+        {
+            get
+            {
+                if (_volume == 0) return 1;
+                return (double)(((double)_outlinks) / _volume);
+            }
+        }
+        public int Size
+        {
+            get { return this.MemberNodes.Count; }
+        }
+        private double _CommCondThres;
+
+        public double CommCondThres
+        {
+            get { return _CommCondThres; }
+            set { _CommCondThres = value; }
+        }
+
+        public Dictionary<string, int> MemberNodes;
+        public Dictionary<string, int> OutlinkedNodes;
+        public Dictionary<string, int> TrappedKeywords;
+
+        public Community()
+        {
+            MemberNodes = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+            OutlinkedNodes = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+            TrappedKeywords = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+            _outlinks = 0;
+            _volume = 0;
+        }
+
+        public class EvalVal
+        {
+            public double cond;
+            public bool memberable;
+            public uint linksToCommunity;
+            public uint linksOutCommunity;
+        }
+
+        public EvalVal EvalEEVertex(EEVertex eev)
+        {
+            EvalVal eval = new EvalVal();
+
+            eval.linksOutCommunity = 0;
+            eval.linksToCommunity = 0;
+
+            foreach (string nbor in eev.Neighbors.Keys)
+            {
+                if (MemberNodes.ContainsKey(nbor))
+                {
+                    eval.linksToCommunity++;
+                }
+                else { eval.linksOutCommunity++; }
+            }
+
+            eval.cond = ((double)this.OutlinksCount + eval.linksOutCommunity - eval.linksToCommunity) / (this.Volume + eev.Neighbors.Count);
+
+            if (eval.cond <= this.Conductance) eval.memberable = true;
+            else if (eval.cond - CommCondThres < this.Conductance) eval.memberable = true;
+            else eval.memberable = false;
+
+            return eval;
+        }
+
+        bool isMember(string entity)
+        {
+            return MemberNodes.ContainsKey(entity);
+        }
+
+        public void AddVertexToCommunity(EEVertex eev, EvalVal eval, double pScore)
+        {
+            if (!eval.memberable) return;
+
+            if (!MemberNodes.ContainsKey(eev.Entity))
+            {
+                MemberNodes.Add(eev.Entity, eev.Neighbors.Count);
+                _pScoreSum += pScore;
+            }
+            this._outlinks += (uint)eval.linksOutCommunity;
+            this._volume += (uint)eev.Neighbors.Count;
+            if (OutlinkedNodes.ContainsKey(eev.Entity)) OutlinkedNodes.Remove(eev.Entity);
+
+            foreach (string nbor in eev.Neighbors.Keys)
+            {
+                string[] kws = eev.Neighbors[nbor].kw.Split(",".ToCharArray());
+                foreach (string kw in kws)
+                {
+                    if (kw != "" && kw != " ")
+                    {
+                        if (!TrappedKeywords.ContainsKey(kw)) TrappedKeywords.Add(kw, 0);
+                        TrappedKeywords[kw]++;
+                    }
+                }
+                if (!isMember(nbor))
+                {
+                    if (!OutlinkedNodes.ContainsKey(nbor))
+                        OutlinkedNodes.Add(nbor, 1/*eev.Neighbors[nbor].Weight*/);
+                }
+            }
+        }
+
+        public void Consolidate(int kwThreshold=-1)
+        {
+            if (kwThreshold < 0)
+            {
+                kwThreshold = (int) Math.Ceiling( Math.Log(Size) );
+            }
+            string[] kws = new string[TrappedKeywords.Count];
+
+            int i = 0;
+            foreach (string kw in TrappedKeywords.Keys)
+            {
+                kws[i++] = kw;
+            }
+            foreach (string kw in kws)
+            {
+                if (TrappedKeywords[kw] < kwThreshold)
+                    TrappedKeywords.Remove(kw);
+            }
         }
     }
 }
